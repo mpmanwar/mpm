e68285bf2887c3b65f58f74918d1ace007cce12c
root <anwar.khan@appsbee.com>
2015-04-30 17:31:25 +0530

    left side changes

--- app/views/layouts/inner_leftside.php
+++ app/views/layouts/inner_leftside.php
@@ -19,7 +19,7 @@
                     </form>
                     <!-- /.search form -->
                     <!-- sidebar menu: : style can be found in sidebar.less -->
-                    <ul class="sidebar-menu">
+                    <!-- <ul class="sidebar-menu">
                         <li class="active">
                             <a href="index.html">
                                 <i class="fa fa-angle-double-right"></i> <span>Client List Organisation</span>
@@ -27,82 +27,56 @@
                         </li>
                         <li>
                             <a href="pages/widgets.html">
-                                <i class="fa fa-angle-double-right"></i> <span>Client List Individual</span> <!--<small class="badge pull-right bg-green">new</small>-->
+                                <i class="fa fa-angle-double-right"></i> <span>Client List Individual</span> <small class="badge pull-right bg-green">new</small>
                             </a>
                         </li>
                         <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-angle-double-right"></i>
                                 <span>Jobs</span>
-                               <!-- <i class="fa fa-angle-left pull-right"></i>-->
+                               
                             </a>
-                           <!-- <ul class="treeview-menu">
-                                <li><a href="pages/charts/morris.html"><i class="fa fa-angle-double-right"></i> Morris</a></li>
-                                <li><a href="pages/charts/flot.html"><i class="fa fa-angle-double-right"></i> Flot</a></li>
-                                <li><a href="pages/charts/inline.html"><i class="fa fa-angle-double-right"></i> Inline charts</a></li>
-                            </ul>-->
+                           
                         </li>
                         <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-angle-double-right"></i>
                                 <span>Notice Board</span>
-                                <!--<i class="fa fa-angle-left pull-right"></i>-->
                             </a>
-                           <!-- <ul class="treeview-menu">
-                                <li><a href="pages/UI/general.html"><i class="fa fa-angle-double-right"></i> General</a></li>
-                                <li><a href="pages/UI/icons.html"><i class="fa fa-angle-double-right"></i> Icons</a></li>
-                                <li><a href="pages/UI/buttons.html"><i class="fa fa-angle-double-right"></i> Buttons</a></li>
-                                <li><a href="pages/UI/sliders.html"><i class="fa fa-angle-double-right"></i> Sliders</a></li>
-                                <li><a href="pages/UI/timeline.html"><i class="fa fa-angle-double-right"></i> Timeline</a></li>
-                            </ul>-->
+                           
                         </li>
                         <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-angle-double-right"></i> <span>Companies HSE Data</span>
-                                <!--<i class="fa fa-angle-left pull-right"></i>-->
+                                
                             </a>
-                           <!-- <ul class="treeview-menu">
-                                <li><a href="pages/forms/general.html"><i class="fa fa-angle-double-right"></i> General Elements</a></li>
-                                <li><a href="pages/forms/advanced.html"><i class="fa fa-angle-double-right"></i> Advanced Elements</a></li>
-                                <li><a href="pages/forms/editors.html"><i class="fa fa-angle-double-right"></i> Editors</a></li>
-                            </ul>-->
+                           
                         </li>
                         <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-angle-double-right"></i> <span>HMRC Authorisations</span>
-                               <!-- <i class="fa fa-angle-left pull-right"></i>-->
+                               
                             </a>
-                           <!-- <ul class="treeview-menu">
-                                <li><a href="pages/tables/simple.html"><i class="fa fa-angle-double-right"></i> Simple tables</a></li>
-                                <li><a href="pages/tables/data.html"><i class="fa fa-angle-double-right"></i> Data tables</a></li>
-                            </ul>-->
+                           
                         </li>
                         <li>
                             <a href="pages/calendar.html">
                                 <i class="fa fa-angle-double-right"></i> <span>CRM</span>
-                                <!--<small class="badge pull-right bg-red">3</small>-->
+                                
                             </a>
                         </li>
                         <li>
                             <a href="pages/mailbox.html">
                                 <i class="fa fa-angle-double-right"></i> <span>Reports</span>
-                                <!--<small class="badge pull-right bg-yellow">12</small>-->
+                                
                             </a>
                         </li>
                         <li class="treeview">
                             <a href="#">
                                 <i class="fa fa-angle-double-right"></i> <span>Contact & Mass Mail</span>
-                                <!--<i class="fa fa-angle-left pull-right"></i>-->
+                                
                             </a>
-                           <!-- <ul class="treeview-menu">
-                                <li><a href="pages/examples/invoice.html"><i class="fa fa-angle-double-right"></i> Invoice</a></li>
-                                <li><a href="pages/examples/login.html"><i class="fa fa-angle-double-right"></i> Login</a></li>
-                                <li><a href="pages/examples/register.html"><i class="fa fa-angle-double-right"></i> Register</a></li>
-                                <li><a href="pages/examples/lockscreen.html"><i class="fa fa-angle-double-right"></i> Lockscreen</a></li>
-                                <li><a href="pages/examples/404.html"><i class="fa fa-angle-double-right"></i> 404 Error</a></li>
-                                <li><a href="pages/examples/500.html"><i class="fa fa-angle-double-right"></i> 500 Error</a></li>
-                                <li><a href="pages/examples/blank.html"><i class="fa fa-angle-double-right"></i> Blank Page</a></li>
-                            </ul>-->
+                           
                         </li>
                          <li>
                             <a href="pages/mailbox.html">
@@ -110,4 +84,4 @@
                                
                             </a>
                         </li>
-                    </ul>
+                    </ul> -->
