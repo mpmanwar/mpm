02ba25990ac4f9fc7b339e2c5164cc1ce9036da5
Anwar Khan <anwar.khan@appsbee.com>
2015-07-30 19:50:27 +0530

    staff chenges

--- app/routes.php
+++ app/routes.php
@@ -242,3 +242,9 @@ Route::any('/vatreturn/manage-tasks', array("as"=>"manage_tasks", "uses"=>'VatRe
 /*=============== Staff Appraisal Section Start =================*/
 Route::any('/staff-appraisal', array('as'=>'index', 'uses'=>'StaffAppraisalController@index'));
 /*=============== Staff Appraisal Section End =================*/
+
+/*==================Time Sheet Start ==============*/
+Route::post('/timesheet/insert-time-sheet', array("as"=>"insert-time-sheet", "uses"=>'TimesheetController@insert_time_sheet'));
+Route::post('/timesheet/timesheet-templates', array("as"=>"timesheet-templates", "uses"=>'TimesheetController@timesheet_templates'));
+Route::post('/timesheet/edit-time-sheet', array("as"=>"edit-time-sheet", "uses"=>'TimesheetController@edit_time_sheet'));
+/*==================Time Sheet End ==============*/
