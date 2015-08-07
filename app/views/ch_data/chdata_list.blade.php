44bc026d9c417ed6cbcf06dcee302798f005b25b
root <anwar.khan@appsbee.com>
2015-08-07 19:39:50 +0530

    client list allocation

--- app/views/ch_data/chdata_list.blade.php
+++ app/views/ch_data/chdata_list.blade.php
@@ -155,7 +155,7 @@ $(function() {
                 <td align="center">{{ isset($details['incorporation_date'])?date("d-m-Y", strtotime($details['incorporation_date'])):"" }}</td>
                 <td align="center">{{ $details['registration_number'] or "" }}</td>
                 <td align="left"><a href="/chdata-details/{{ $details['registration_number'] }}">{{ $details['business_name'] or "" }}</a></td>
-                <td align="center">{{ $details['acc_ref_day'] or "" }}/{{ $details['ref_month'] or "" }}</td>
+                <td align="center">{{ $details['acc_ref_day'] or "" }}-{{ $details['ref_month'] or "" }}</td>
                 <td align="center">{{ $details['ch_auth_code'] or "" }}</td>
                 <td align="center">{{ isset($details['last_acc_madeup_date'])?date("d-m-Y", strtotime($details['last_acc_madeup_date'])):"" }}</td>
                 <td align="center">{{ isset($details['next_ret_due'])?date("d-m-Y", strtotime($details['next_ret_due'])):"" }}</td>
