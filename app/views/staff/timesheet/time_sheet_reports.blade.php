7ed3007c4b9931307d22e33eeb5922f581d2a740
Anwar Khan <anwar.khan@appsbee.com>
2015-08-12 22:03:32 +0530

    client staff report

--- app/views/staff/timesheet/time_sheet_reports.blade.php
+++ app/views/staff/timesheet/time_sheet_reports.blade.php
@@ -474,64 +474,9 @@ function openstaffModal(str_id) {
                     </div>
                   </div>
                   <!--end table-->
-                </div>
-                <!-- /.tab-pane -->
-                <div id="tab_2" class="tab-pane">
-                  <div class="box-body table-responsive">
-                    <div role="grid" class="dataTables_wrapper form-inline" id="example2_wrapper">
-                      <div class="row">
-                        <div class="col-xs-6"></div>
-                        <div class="col-xs-6"></div>
-                      </div>
-                      <div class="row">
-                        <div class="col-xs-12">
-						<?php 
-						//echo '<pre>';
-						//print_r($time_sheet_report);
-							
-						?>
-                          <table class="table table-bordered table-hover dataTable" id="example2" aria-describedby="example2_info">
-            
-                            <thead>
-                              <tr role="row">
-                                <th align="center"><input type="checkbox" id="allCheckSelect"/></th>
-                                <th align="center"><strong>Date</strong></th>
-                                <th align="center"><strong>Staff Name</strong></th>
-                                <th><strong>Client Name</strong></th>
-                                <th align="left"><strong>Service</strong></th>
-                                <th><strong>HRS</strong></th>
-                                <th><strong>Notes</strong></th>
-                                <th><strong>Action</strong></th>
-                              </tr>
-                            </thead>
-
-                            <tbody role="alert" aria-live="polite" aria-relevant="all">
-							
-							@if(!empty($time_sheet_report))
-								  @foreach($time_sheet_report as $key=>$staff_row)
-								 <tr>
-									<td align="center"><input type="checkbox" /></td>
-									<td align="center">{{ $staff_row['created_date'] }}</td>
-									<td align="center">{{ $staff_row['staff_detail']['fname'] }} {{ $staff_row['staff_detail']['lname'] }}</td>
-									<td  align="left">{{ $staff_row['client_detail']['field_value'] }}</td>
-									<td align="left">{{ $staff_row['old_vat_scheme']['vat_scheme_name'] }}</td>
-									<td align="center">{{ $staff_row['hrs'] }}</td>
-									<td align="center">{{ $staff_row['notes'] }}</td>
-									<td align="center"><a href="#" data-toggle="modal" data-template_id="{{ $staff_row['timesheet_id'] }}" onclick="openModal('{{ $staff_row['timesheet_id'] }}')"><img src="/img/edit_icon.png" width="15"></a>
-									</tr>
-									@endforeach
-								@endif
-                                  
-                              
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!-- /.tab-pane -->
-              </div>
+                  
+                  
+                  <div>
               <p class="btn btn-default">Client Time Report</p>
               
               <table class="table table-bordered table-hover dataTable" id="example3" aria-describedby="example2_info">
@@ -610,9 +555,71 @@ function openstaffModal(str_id) {
                               
                             </tbody>
                           </table>
+              </div>
+                </div>
+                
+                <!-- /.tab-pane -->
+                <div id="tab_2" class="tab-pane">
+                  <div class="box-body table-responsive">
+                    <div role="grid" class="dataTables_wrapper form-inline" id="example2_wrapper">
+                      <div class="row">
+                        <div class="col-xs-6"></div>
+                        <div class="col-xs-6"></div>
+                      </div>
+                      <div class="row">
+                        <div class="col-xs-12">
+						<?php 
+						//echo '<pre>';
+						//print_r($time_sheet_report);
+							
+						?>
+                          <table class="table table-bordered table-hover dataTable" id="example2" aria-describedby="example2_info">
+            
+                            <thead>
+                              <tr role="row">
+                                <th align="center"><input type="checkbox" id="allCheckSelect"/></th>
+                                <th align="center"><strong>Date</strong></th>
+                                <th align="center"><strong>Staff Name</strong></th>
+                                <th><strong>Client Name</strong></th>
+                                <th align="left"><strong>Service</strong></th>
+                                <th><strong>HRS</strong></th>
+                                <th><strong>Notes</strong></th>
+                                <th><strong>Action</strong></th>
+                              </tr>
+                            </thead>
+
+                            <tbody role="alert" aria-live="polite" aria-relevant="all">
+							
+							@if(!empty($time_sheet_report))
+								  @foreach($time_sheet_report as $key=>$staff_row)
+								 <tr>
+									<td align="center"><input type="checkbox" /></td>
+									<td align="center">{{ $staff_row['created_date'] }}</td>
+									<td align="center">{{ $staff_row['staff_detail']['fname'] }} {{ $staff_row['staff_detail']['lname'] }}</td>
+									<td  align="left">{{ $staff_row['client_detail']['field_value'] }}</td>
+									<td align="left">{{ $staff_row['old_vat_scheme']['vat_scheme_name'] }}</td>
+									<td align="center">{{ $staff_row['hrs'] }}</td>
+									<td align="center">{{ $staff_row['notes'] }}</td>
+									<td align="center"><a href="#" data-toggle="modal" data-template_id="{{ $staff_row['timesheet_id'] }}" onclick="openModal('{{ $staff_row['timesheet_id'] }}')"><img src="/img/edit_icon.png" width="15"></a>
+									</tr>
+									@endforeach
+								@endif
+                                  
+                              
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.tab-pane -->
+              </div>
               
               
             </div>
+            
+            
           </div>
         </form>
       </div>
