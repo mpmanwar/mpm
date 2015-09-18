33280ac63da17b4206521cf095ca461244d1d7c4
root <anwar.khan@appsbee.com>
2015-09-18 13:58:07 +0530

    crm leads

--- app/views/crm/index.blade.php
+++ app/views/crm/index.blade.php
@@ -107,10 +107,7 @@ $(function() {
     });
     
     
-    
-    
-    
-     $('#exampletab2').dataTable({
+    $('#exampletab2').dataTable({
       "bPaginate": true,
       "bLengthChange": true,
       "bFilter": true,
@@ -164,9 +161,6 @@ $(function() {
       ],
       "aaSorting": [[1, 'desc']]
     });
-    
-    
-    
     
     $('#example611').dataTable({
       "bPaginate": true,
@@ -860,152 +854,7 @@ $(function() {
 
 <!-- Tab 5 Start-->
   <div id="tab_51" class="tab-pane {{ ($page_open == 51 || $page_open == 511 || $page_open == 512 || $page_open == 513)?'active':'' }}">
-
-    <!-- <div class="tab_topcon">
-      <div class="top_bts" style="float:left;">
-        <ul style="padding:0;">
-          <li>
-            <a class="btn btn-danger deleteLeads" href="javascript:void(0)">DELETE</a>
-          </li>
-          <li>
-            <div class="import_fromch_main" style="width:182px;">
-              <div class="import_fromch">
-                <a href="javascript:void(0)" class="import_fromch_link">+ NEW LEAD</a>
-                <a href="javascript:void(0)" class="i_selectbox" id="select_new_lead"><img src="/img/arrow_icon.png"></a>
-                <div class="clearfix"></div>
-              </div>
-              <div class="crm_dropdown open_toggle" style="left: 90px;">
-              <ul>
-                <li><href="javascript:void(0)" data-type="ind" data-leads_id="0" class="open_new_lead-modal">Individual</a></li>
-                <li><href="javascript:void(0)" data-type="org" data-leads_id="0" class="open_new_lead-modal">Organisation</a></li>
-              </ul>
-            </div>
-          </div>
-          </li>
-        <div class="clearfix"></div>
-        </ul>
-      </div>
-      
-      <div class="clearfix"></div>
-    </div> -->
-
-    <ul class="leads_tab" style="border: none;">
-        <li style="width:8%">
-          <a class="btn btn-danger deleteLeads" href="javascript:void(0)">DELETE</a>
-        </li>
-        <li>
-          <div class="import_fromch_main" style="width:182px;">
-            <div class="import_fromch">
-              <a href="javascript:void(0)" class="import_fromch_link">+ NEW LEAD</a>
-              <a href="javascript:void(0)" class="i_selectbox" id="select_new_lead"><img src="/img/arrow_icon.png"></a>
-              <div class="clearfix"></div>
-            </div>
-            <div class="crm_dropdown open_toggle" style="left: 90px;">
-            <ul>
-              <li><href="javascript:void(0)" data-type="ind" data-leads_id="0" class="open_new_lead-modal">Individual</a></li>
-              <li><href="javascript:void(0)" data-type="org" data-leads_id="0" class="open_new_lead-modal">Organisation</a></li>
-            </ul>
-          </div>
-        </div>
-        </li>
-
-        <li style="width:9%;" class="{{ ($page_open == '51')?'active_leads':'' }}"><a href="{{ $goto_url }}/{{ base64_encode('51') }}/{{ base64_encode($owner_id) }}"><h3 style="background:#0066FF;">All [<span id="task_count_11">10</span>]</h3></a>
-          <p>100%</p>
-        </li>
-
-        @if(isset($leads_tabs) && count($leads_tabs) >0)
-        <?php $j = 11;?>
-          @foreach($leads_tabs as $key=>$tab_row)
-            @if(isset($tab_row['is_show']) && $tab_row['is_show'] == 'L')
-            <li class="{{ ($page_open == '5'.$j)?'active_leads':'' }}"><a href="{{ $goto_url }}/{{ base64_encode('5'.$j) }}/{{ base64_encode($owner_id) }}"><h3 style="background:#{{ $tab_row['color_code'] or "" }};"><span id="step_field_{{ $tab_row['tab_id'] or "" }}">{{ $tab_row['tab_name'] or "" }}</span> [<span id="task_count_1{{ $j }}">{{ $tab_row['count'] or "0" }}</span>]</h3></a>
-            <p>{{ (isset($tab_row['table_value']['total']) && $all_total != '0.00')?round(str_replace(',','',$tab_row['table_value']['total'])*100/str_replace(',','',$all_total), 2):'0.00' }}%</p>
-          </li>
-            <?php $j++;?>
-            @endif
-          @endforeach
-        @endif
-
-        <div class="clearfix"></div>
-    </ul>
-    
-  <div class="tab-content">
-
-    <div id="tab_51" class="tab-pane top_margin {{ ($page_open == '51')?'active':'' }}">
-      <table class="table table-bordered table-hover dataTable crm" id="example5" aria-describedby="example5_info">
-      <thead>
-        <tr role="row">
-          <th width="3%"><input type='checkbox' class="CheckallCheckbox"></th>
-          <th width="7%">Date</th>
-          <th width="12%">Deal Owner</th>
-          <th width="12%">Prospect Name</th>
-          <th>Contact Name</th>
-          <th width="5%">Phone</th>
-          <th width="13%">Convert to Opportunity</th>
-          <th width="8%">Lead Source</th>
-          <th width="9%">Lead Status <a href="javascript:void(0)" class="lead_status-modal" style="float:right;" data-is_show="L"><i class="fa fa-cog fa-fw" style="color:#00c0ef"></i></a></th>
-          <th width="5%">Notes</th>
-        </tr>
-      </thead>
-
-      <tbody role="alert" aria-live="polite" aria-relevant="all">
-        @if(isset($leads) && count($leads) >0)
-          @foreach($leads as $key=>$leads_row)
-            <tr {{ ($leads_row['show_archive'] == "Y")?'style="background:#ccc"':"" }}>
-              <td><input type='checkbox' data-archive="{{ $leads_row['show_archive'] }}" class="ads_Checkbox" name="leads_delete_id[]" value="{{ $leads_row['leads_id'] or "" }}"></td>
-              <td align="left">{{ $leads_row['date'] or "" }}</td>
-              <td align="left">{{ $leads_row['deal_owner'] or "" }}</td>
-              <td align="left">
-                @if(isset($leads_row['client_type']) && $leads_row['client_type'] == "org")
-                  <a href="javascript:void(0)" data-type="org" data-leads_id="{{ $leads_row['leads_id'] }}" class="open_new_lead-modal">{{ $leads_row['prospect_name'] or "" }}</a>
-                @else
-                  <a href="javascript:void(0)" data-type="ind" data-leads_id="{{ $leads_row['leads_id'] or "" }}" class="open_new_lead-modal">{{ $leads_row['prospect_title'] or "" }} {{ $leads_row['prospect_fname'] or "" }} {{ $leads_row['prospect_lname'] or "" }}</a>
-                @endif
-              </td>
-              <td align="left">{{ $leads_row['contact_title'] or "" }} {{ $leads_row['contact_fname'] or "" }} {{ $leads_row['contact_lname'] or "" }}</td>
-              <td align="center">Phone</td>
-              <td align="center">
-                <select class="form-control newdropdown status_dropdown" id="11_status_dropdown_{{ $leads_row['leads_id'] or "" }}" data-leads_id="{{ $leads_row['leads_id'] or "" }}">
-                  <option value="no">No</option>
-                  <option value="yes">Yes</option>
-                </select>
-              </td>
-              
-              <td align="center"></td>
-              <td align="center">
-                <select class="form-control newdropdown status_dropdown" id="11_status_dropdown_{{ $leads_row['leads_id'] or "" }}" data-leads_id="{{ $leads_row['leads_id'] or "" }}">
-                  @if(isset($leads_tabs) && count($leads_tabs) >0)
-                    @foreach($leads_tabs as $key=>$tab_row)
-                      @if(isset($tab_row['is_show']) && $tab_row['is_show'] == 'L')
-                        <option value="{{ $tab_row['tab_id'] or "" }}" {{ (isset($leads_row['lead_status']) && $leads_row['lead_status'] == $tab_row['tab_id'])?'selected':'' }}>{{ $tab_row['tab_name'] or "" }}</option>
-                        @endif
-                    @endforeach
-                  @endif
-                </select>
-              </td>
-              <td>
-                <input type="hidden" id="notes_{{ $leads_row['leads_id'] or "" }}" value="{{ $leads_row['notes'] or "" }}">
-                <a href="javascript:void(0)" class="notes_btn open_notes_popup" data-leads_id="{{ $leads_row['leads_id'] or "" }}" data-tab="51"><span style="{{ (isset($leads_row['notes']) && $leads_row['notes'] != '')?'border-bottom:3px dotted #3a8cc1 !important':''}}">notes</span></a>
-              </td>
-              
-            </tr>
-          @endforeach
-        @endif
-        
-      </tbody>
-    </table>
-    </div>
-
-    @for($k=11; $k <=13;$k++)                          
-    <div id="tab_5{{$k}}" class="tab-pane top_margin {{ ($page_open == '5'.$k)?'active':'' }}">
-      {{$k}}
-    </div>
-    @endfor  
-
-
-
-  </div>
-
-
+      @include('crm/leads_tab')
   </div>
 <!-- Tab 5 End-->
 
@@ -1050,8 +899,8 @@ $(function() {
                               </div>
                               <div class="crm_dropdown open_toggle">
                               <ul>
-                                <li><href="javascript:void(0)" data-type="ind" data-leads_id="0" class="open_form-modal">Individual</a></li>
-                                <li><href="javascript:void(0)" data-type="org" data-leads_id="0" class="open_form-modal">Organisation</a></li>
+                                <li><a href="javascript:void(0)" data-type="ind" data-leads_id="0" class="open_form-modal">Individual</a></li>
+                                <li><a href="javascript:void(0)" data-type="org" data-leads_id="0" class="open_form-modal">Organisation</a></li>
                               </ul>
                             </div>
                           </div>
